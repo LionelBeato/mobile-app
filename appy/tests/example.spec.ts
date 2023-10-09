51efa592c0779f1ac04676a77712db16b4eeb17e
lionel.beato <lionel.beato@gmail.com>
2023-10-09 10:29:45 -0400

    adds ci pipeline for playwright, tweak config

---
+++ appy/tests/example.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('app has title Ionic App', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page).toHaveTitle(/Ionic App/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
